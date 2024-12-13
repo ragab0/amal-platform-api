08839f48bd12c64de5cfdd0097fdf03d74fb1b1e
ragab0 <legendragab@gmail.com>
2024-12-13 02:37:44 +0200

    RESTORE again - even un secure not work !

--- configs/headerCookies.js
+++ configs/headerCookies.js
@@ -1,13 +1,10 @@
 const { NODE_ENV } = process.env;
 
-// FOR DEV -> TILL WE GOT ON A DOMAIN WITH SUBDOMAIN TO NOT FAIL ON THIRD-PARTY
-// WHICH NOT WORK WITH ME ON PRODUCTION !!;
 const COOKIE_CONFIG = {
-  // httpOnly: true,
-  // secure: NODE_ENV === "production",
-  // sameSite: NODE_ENV === "production" ? "none" : "lax", // Required for cross-origin (different domains) requests
-  // domain: NODE_ENV === "development" ? "localhost" : ".vercel.app",
-  // path: "/",
+  httpOnly: true,
+  secure: NODE_ENV === "production",
+  sameSite: NODE_ENV === "production" ? "none" : "lax", // Required for cross-origin (different domains) requests
+  path: "/",
 };
 
 module.exports = { COOKIE_CONFIG };
