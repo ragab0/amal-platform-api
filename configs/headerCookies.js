3ea4162dc39fc2da55b6e3f5db43cba888ee44a6
ragab0 <legendragab@gmail.com>
2024-12-13 02:20:23 +0200

    DROP COOKI SECURITY confgis FOR DEV

--- configs/headerCookies.js
+++ configs/headerCookies.js
@@ -1,11 +1,13 @@
 const { NODE_ENV } = process.env;
 
+// FOR DEV -> TILL WE GOT ON A DOMAIN WITH SUBDOMAIN TO NOT FAIL ON THIRD-PARTY
+// WHICH NOT WORK WITH ME ON PRODUCTION !!;
 const COOKIE_CONFIG = {
-  httpOnly: true,
-  secure: NODE_ENV === "production",
-  sameSite: NODE_ENV === "production" ? "none" : "lax", // Required for cross-origin (different domains) requests
-  domain: NODE_ENV === "development" ? "localhost" : ".vercel.app",
-  path: "/",
+  // httpOnly: true,
+  // secure: NODE_ENV === "production",
+  // sameSite: NODE_ENV === "production" ? "none" : "lax", // Required for cross-origin (different domains) requests
+  // domain: NODE_ENV === "development" ? "localhost" : ".vercel.app",
+  // path: "/",
 };
 
 module.exports = { COOKIE_CONFIG };
