2b2750ce8727b5c8bccc17091f935cba99518047
ragab0 <legendragab@gmail.com>
2024-12-13 02:51:15 +0200

    Resotre our cookie config domain field

--- configs/headerCookies.js
+++ configs/headerCookies.js
@@ -4,6 +4,7 @@ const COOKIE_CONFIG = {
   httpOnly: true,
   secure: NODE_ENV === "production",
   sameSite: NODE_ENV === "production" ? "none" : "lax", // Required for cross-origin (different domains) requests
+  domain: NODE_ENV === "production" ? "amal-dev.vercel.app" : "localhost",
   path: "/",
 };
 
