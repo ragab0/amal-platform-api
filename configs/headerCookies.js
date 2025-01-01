f636018756518b1e0ba2cdf8cca0fe650ee4cf3e
ragab0 <legendragab@gmail.com>
2025-01-01 18:26:12 +0200

    Disable sameSite too

--- configs/headerCookies.js
+++ configs/headerCookies.js
@@ -9,6 +9,7 @@ const { NODE_ENV, FRONTEND_URL } = process.env;
 
 const COOKIE_CONFIG = {
   httpOnly: false,
+  sameSite: NODE_ENV === "production" ? "none" : "lax", // Required for cross-origin (different domains) requests
 };
 
 module.exports = { COOKIE_CONFIG };
