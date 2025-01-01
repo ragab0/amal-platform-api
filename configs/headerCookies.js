46ef385e04795294854d9d352bd0d73b1d544220
ragab0 <legendragab@gmail.com>
2025-01-01 18:20:25 +0200

    Disable httpOnly cookie - git add configsJUST FOR NOWgit add configs - TILL WE GET ON A DOMAIN

--- configs/headerCookies.js
+++ configs/headerCookies.js
@@ -1,10 +1,14 @@
 const { NODE_ENV, FRONTEND_URL } = process.env;
 
+// const COOKIE_CONFIG = {
+//   httpOnly: true,
+//   secure: NODE_ENV === "production",
+//   sameSite: NODE_ENV === "production" ? "none" : "lax", // Required for cross-origin (different domains) requests
+//   path: "/",
+// };
+
 const COOKIE_CONFIG = {
-  httpOnly: true,
-  secure: NODE_ENV === "production",
-  sameSite: NODE_ENV === "production" ? "none" : "lax", // Required for cross-origin (different domains) requests
-  path: "/",
+  httpOnly: false,
 };
 
 module.exports = { COOKIE_CONFIG };
